aebe584fa4209d0adbd935cceab7d2e347b8ddb7
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-21 12:09:26 -0500

    90%

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const enviar = document.querySelector("#enviar");
-
 const email = document.querySelector("#email");
 const asunto = document.querySelector("#asunto");
 const mensaje = document.querySelector("#mensaje");
-
+const form = document.querySelector("#enviar-mail");
+const er =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 eventListeners();
 function eventListeners() {
   document.addEventListener("DOMContentLoaded", startApp);
@@ -19,8 +20,60 @@ function startApp() {
 
 function validateFrom(e) {
   if (e.target.value.length > 0) {
-    console.log("Sik tengo");
+    const error = document.querySelector("p.error");
+    if (error) {
+      error.remove();
+    }
+
+    e.target.classList.remove("border", "border-red-500");
+    e.target.classList.add("border", "border-green-500");
   } else {
+    e.target.classList.remove("border", "border-green-500");
     e.target.classList.add("border", "border-red-500");
+    showError("All fields are requierd");
+  }
+
+  if (e.target.type === "email") {
+    // const results = e.target.value.indexOf("@");
+    if (er.test(e.target.value)) {
+      const error = document.querySelector("p.error");
+      if (error) {
+        error.remove();
+      }
+    } else {
+      e.target.classList.add("border", "border-red-500");
+      showError("It must be a valid email");
+    }
+  }
+  if (er.test(email.value) && asunto.value !== "" && mensaje.value !== "") {
+    enviar.ariaDisabled = false;
+    enviar.classList.remove("cursor-not-allowed", "opacity-50");
+  }
+}
+
+// } else if (e.target.type === text) {
+//   console.log("Soy text");
+// } else {
+//   console.log("Textaera");
+// }
+
+function showError(message) {
+  const messageError = document.createElement("p");
+  messageError.textContent = message;
+  messageError.classList.add(
+    "border",
+    "border-red-500",
+    "background-color-100",
+    "text-red-500",
+    "p-3",
+    "mt-5",
+    "text-center",
+    "error"
+  );
+
+  const errors = document.querySelectorAll(".error");
+
+  if (errors.length === 0) {
+    form.appendChild(messageError);
   }
 }
